6befec0f2ce4268c79d8da444cf2bf4aeb219a99
Jonathan Birkholz <rookieone@gmail.com>
2015-01-30 19:31:24 -0600

    watchify browserify webserver livereload

--- gulpFile.js
+++ gulpFile.js
@@ -1,16 +1,36 @@
-var gulp = require("gulp");
-var webserver = require("gulp-webserver");
+var gulp = require("gulp")
+var webserver = require("gulp-webserver")
+var browserify = require("browserify")
+var watchify = require('watchify')
+var source = require('vinyl-source-stream')
+var livereload = require('gulp-livereload')
 
 gulp.task("move-files", function() {
   return gulp.src("app/*.html")
-    .pipe(gulp.dest("build"));
-});
+    .pipe(gulp.dest("build"))
+})
 
-gulp.task("webserver", ["move-files"], function() {
+gulp.task("build-app", function() {
+  var bundle = function() {
+    return bundler
+      .bundle()
+      .pipe(source("main.js"))
+      .pipe(gulp.dest("./build"))
+      .pipe(livereload())
+  }
+
+  var bundler = browserify("./app/main.js", watchify.args)
+  bundler.on("update", bundle)
+  bundler = watchify(bundler)
+
+  return bundle()
+})
+
+gulp.task("webserver", ["move-files", "build-app"], function() {
   return gulp.src("build")
     .pipe(webserver({
       livereload: true
-    }));
-});
+    }))
+})
 
-gulp.task("default", ["move-files", "webserver"]);
+gulp.task("default", ["move-files", "webserver"])
